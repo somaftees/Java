bc9a64c5b99e0d42923967e726200c87581bba76
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-02-28 18:12:34 +0200

    done

--- upcasting/src/shapes/rect.java
+++ upcasting/src/shapes/rect.java
@@ -3,7 +3,7 @@ package shapes;
 public class rect extends shape{
 	
 	public void printshape() {
-		System.out.println("****\n****\n****\n****\n****");
+		System.out.println("******\n******\n******\n******");
 	}
 
 }
