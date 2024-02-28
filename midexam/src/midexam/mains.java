3679fb8390485e6714918f1ce11f6a450343061e
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-02-28 15:02:05 +0200

    done

--- midexam/src/midexam/mains.java
+++ midexam/src/midexam/mains.java
@@ -13,13 +13,13 @@ public class mains {
 		s1.setinfo("tasnim", 21, 911156342, 2003);
 		s1.getinfo();
 		
-		s2.setinfo("retag", 22, 911156353, 2002);
+		s2.setinfo("takwa", 20, 911156353, 2004);
 		s2.getinfo();
 		
-		s3.setinfo("aya", 22, 91128003, 2002);
+		s3.setinfo("tana", 19, 91128003, 2005);
 		s3.getinfo();
 		
-		s4.setinfo("soma", 21, 9111111, 2003);
+		s4.setinfo("raghad", 13, 9111111, 2011);
 		s4.getinfo();
 		
 		student.printTotalNum();
